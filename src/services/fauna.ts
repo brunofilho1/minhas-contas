f005bf042e3b2ab36afd86ef79f3e2ddbab8f6d3
Bruno Filho <oliveira.brunofilho@gmail.com>
2022-06-06 23:21:24 -0300

    change .env environment

--- src/services/fauna.ts
+++ src/services/fauna.ts
@@ -1,6 +1,7 @@
 import { Client } from "faunadb";
 
 export const fauna = new Client({
-  secret: process.env.FAUNADB_KEY,
+  // @ts-ignore
+  secret: import.meta.env.VITE_FAUNADB_KEY,
   domain: "db.us.fauna.com",
 });
